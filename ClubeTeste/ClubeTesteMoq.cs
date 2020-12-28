d60ab956050670e4e8f11ab1bdd7416a295e8fd5
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2020-12-28 10:02:04 -0300

    Add project files.

---
+++ ClubeTeste/ClubeTesteMoq.cs
@@ -0,0 +1,13 @@
+﻿using Clube_Beca;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace ClubeTeste
+{
+    [TestClass]
+    public class ClubeTesteMoq
+    {
+        private readonly Clube _target;
+        private readonly Mock<ISocio> _mock;
+    }
+}
---
+++ Clube_Beca/Socio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clube_Beca
+{
+    public class Socio
+    {
+        public int SocioID {​​ get; set; }​​
+        public string Nome {​​ get; set; }​​
+        public string Sobrenome {​​ get; set; }​​
+        public int Limite {​​ get; set; }​​
+        public int Idade {​​ get; set; }​​
+    }
+}
