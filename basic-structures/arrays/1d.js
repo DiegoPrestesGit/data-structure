2dc63e29a993bd21cb56e946f0e7d8fbecc9e1ed
Diego Prestes <diegoprestes.git@hotmail.com>
2022-07-29 10:24:33 -0300

    messin around

--- basic-structures/arrays/1d.js
+++ basic-structures/arrays/1d.js
@@ -6,5 +6,24 @@ const array = [10, 20, 23, 1, 31, 100, 21, 101, 64, 12];
  * @param {array<number>} arr
  */
 const reduceArr = (arr) => arr.reduce((prev, currentVal) => prev + currentVal);
-
 // console.log(reduceArr(array));
+
+/**
+ * @description get an array and a 'n', returns the first n items in that array
+ * @param {array<number>} arr
+ * @param {number} n - number of items to take of the array, in sequential order, starting from index zero[0]
+ */
+const firstN = (arr, n = 0) => {
+  if (arr.length === 0) return [];
+  if (n <= 1) return arr[0];
+
+  const newArr = [];
+  for (let i = 0; i < n; i++) {
+    if (!arr[i]) break;
+
+    newArr.push(arr[i]);
+  }
+  return newArr;
+};
+
+console.log(firstN(array, 20));
