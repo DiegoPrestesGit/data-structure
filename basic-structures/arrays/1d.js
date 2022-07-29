d150465f8ee59c999e43a0db3b207dab2ba006de
Diego Prestes <diegoprestes.git@hotmail.com>
2022-07-29 10:05:49 -0300

    messin around

---
+++ basic-structures/arrays/1d.js
@@ -0,0 +1,10 @@
+// one dimention arrays
+
+const array = [10, 20, 23, 1, 31, 100, 21, 101, 64, 12];
+
+/**
+ * @param {array<number>} arr
+ */
+const reduceArr = (arr) => arr.reduce((prev, currentVal) => prev + currentVal);
+
+// console.log(reduceArr(array));
