8296b08ce97d00c7fea6abaac4f7aef29751398e
diegolikescode <diegoprestes.git@hotmail.com>
2023-06-28 21:31:36 -0300

    feat: day1 of advent of code 2015

---
+++ src/day01/A.java
@@ -0,0 +1,47 @@
+package day01;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class A {
+
+	/**
+	 *
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		String fileName = "./input.txt";
+		String contents = stringFromFile(fileName);
+		String[] strArr = contents.split("");
+
+		int floorsCount = 0;
+		int currentFloor = 0;
+
+		for (int i = 0; i < strArr.length; i++) {
+			floorsCount++;
+			if (strArr[i].equals("(")) {
+				currentFloor++;
+			} else {
+				currentFloor--;
+			}
+			if (currentFloor == -1) {
+				System.out.println(floorsCount);
+				break;
+			}
+		}
+	}
+
+	private static String stringFromFile(String filePath) throws IOException {
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			String line = reader.readLine();
+
+			reader.close();
+			return line;
+		} catch (Exception e) {
+			throw new IOException("cannot find file");
+		}
+	}
+}
--- src/main/java/com/dataStructures/basicStuff/Basic001.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.dataStructures.basicStuff;
-
-public class Basic001 {
-	public static void main(String[] args) {
-		System.out.println("yes, indeed");
-	}
-}
