de9f58847f094365a5367949ab8fe040df586f81
diegolikescode <diegoprestes.git@hotmail.com>
2023-10-24 12:37:08 -0300

    y2015/day05B

--- y2015/day05.go
+++ y2015/day05.go
@@ -34,3 +34,40 @@ func (m SharedType) Day05_A(testFile string) {
 	fmt.Println("nice ones", niceOnes)
 }
 
+func Check2letters2times (str string) (bool) {
+	for i:=1; i<len(str);i++ {
+		var repeatedOne string = string(str[i-1])+string(str[i])
+		for j := i+2; j<len(str);j++ {
+			verifying := string(str[j-1])+string(str[j])
+			if repeatedOne == verifying {
+				fmt.Println(str, repeatedOne)
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func CheckSameLetterWith1inBetween (str string) (bool) {
+	for i:=1; i < len(str)-1;i++ {
+		if str[i-1] == str[i+1] {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (m SharedType) Day05_B(testFile string) {
+	str := ReturnFileByLine(testFile)
+	niceOnes := 0
+
+	for _, v := range str {
+		if CheckSameLetterWith1inBetween(v) && Check2letters2times(v) {
+			niceOnes++
+		}
+	}
+
+	fmt.Println("nice ones", niceOnes)
+}
