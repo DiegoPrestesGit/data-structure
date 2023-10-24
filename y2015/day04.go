c8f667711461d8cbf2d1af44bb29937183c1bc60
diegolikescode <diegoprestes.git@hotmail.com>
2023-10-24 08:44:18 -0300

    y2015/day04B of AoC

--- y2015/day04.go
+++ y2015/day04.go
@@ -24,3 +24,23 @@ func (m SharedType) Day04_A(testFile string) {
 		count++
 	}
 }
+
+func (m SharedType) Day04_B(testFile string) {
+	str := ReturnFileByLine(testFile)[0]
+	fmt.Println(str)
+
+	count := 0
+	for true {
+		data := []byte(str+strconv.Itoa(count))
+		hash := md5.Sum(data)
+		hashStr := fmt.Sprintf("%x", hash)
+
+		if hashStr[:6] == "000000" {
+			fmt.Println()
+			fmt.Println(count)
+			break
+		}
+		count++
+	}
+}
+
