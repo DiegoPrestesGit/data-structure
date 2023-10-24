57401ebdbd49a58c8c9268e6c0abdf6bc1c7ea4f
diegolikescode <diegoprestes.git@hotmail.com>
2023-10-24 08:39:30 -0300

    y2015/day04A of AoC

---
+++ y2015/day04.go
@@ -0,0 +1,26 @@
+package y2015
+
+import (
+	"crypto/md5"
+	"fmt"
+	"strconv"
+)
+
+func (m SharedType) Day04_A(testFile string) {
+	str := ReturnFileByLine(testFile)[0]
+	fmt.Println(str)
+
+	count := 0
+	for true {
+		data := []byte(str+strconv.Itoa(count))
+		hash := md5.Sum(data)
+		hashStr := fmt.Sprintf("%x", hash)
+
+		if hashStr[:5] == "00000" {
+			fmt.Println()
+			fmt.Println(count)
+			break
+		}
+		count++
+	}
+}
